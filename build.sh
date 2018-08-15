4c2e936a748432deead61d3eeb59c20ee9ee443f
Jessica <jtstodola@gmail.com>
2018-08-14 22:52:12 -0500

    Add comments

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/use/bin/env bash
 
+# Install npm packages and minify css
 make build
 make minify-css 
+
+# Create public build directory for deployment
 mkdir public
 rsync -a src node_modules public
+
 echo 'Build complete. Ready for deployment!'
