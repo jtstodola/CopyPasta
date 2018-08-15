b30bb0075917f41627c8395d543e3f7a9ba71841
Jessica <jtstodola@gmail.com>
2018-08-14 23:21:45 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-#!/use/bin/env bash
+#!/usr/bin/env bash
+chmod +x build.sh
 
 # Install npm packages and minify css
 make build
 make minify-css 
 
 # Create public build directory for deployment
-mkdir public
+mkdir -p public
 rsync -a src node_modules public
 
 echo 'Build complete. Ready for deployment!'
