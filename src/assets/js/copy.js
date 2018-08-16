1e2ba1730463d4f609edcd882e2d214448f241cd
Jessica <jtstodola@gmail.com>
2018-08-15 22:32:53 -0500

    Replace list of inputs with tabs

--- src/assets/js/copy.js
+++ src/assets/js/copy.js
@@ -2,3 +2,17 @@ function copy(id) {
   document.getElementById(id).select();
   document.execCommand('copy');
 }
+
+function showTab(id) {
+  var tabId = id + "-tab",
+      tabPane = document.getElementById(id),
+      tab = document.getElementById(tabId);
+  
+  document.querySelectorAll('.active').forEach(function (item) {
+    item.classList.remove('active', 'show');
+  });
+  
+  tab.classList.add("active");
+  tabPane.classList.add("show", "active"); 
+}
+
