0747392865168e6c7cafb472e957c1a86b6ba54d
Jessica <jtstodola@gmail.com>
2018-08-16 00:47:23 -0500

    Add tooltip/copied confirmation

--- src/assets/js/copy.js
+++ src/assets/js/copy.js
@@ -1,6 +1,18 @@
 function copy(id) {
-  document.getElementById(id).select();
+  var span = document.createElement('span'),
+      textNode = document.createTextNode("Copied!"),
+      input = id + "-input",
+      buttonId = id + "-btn",
+      buttonDiv = document.getElementById(buttonId),
+      div = document.createElement('div');
+  
+  document.getElementById(input).select();
   document.execCommand('copy');
+  
+  buttonDiv.appendChild(div);
+  div.appendChild(span);
+  span.appendChild(textNode);
+  span.setAttribute("style", "margin-left: .3em; position: absolute; background-color: rgba(108, 117, 125, 1); color: rgba(255, 255, 255, 1); padding: .3em; border: 1px black solid; border-radius: .2em;");
 }
 
 function showTab(id) {
