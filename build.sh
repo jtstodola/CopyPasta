8072a55c6396b110a33ea738f8b88dfec4d45244
Jessica <jtstodola@gmail.com>
2018-08-17 17:32:16 -0500

    Add image compression to build script

--- build.sh
+++ build.sh
@@ -5,6 +5,19 @@ chmod +x build.sh
 make build
 make minify
 
+# Image compression
+cd zopfli
+make zopflipng
+images_dir=../src/assets/images
+
+for image_file in $images_dir/uncompressed/*
+do
+	if [ ! -e $images_dir/compressed/$(basename $image_file) ]; then
+		./zopflipng ../src/assets/images/uncompressed/$(basename $image_file) ../src/assets/images/compressed/$(basename $image_file)
+	fi
+done
+cd ../
+
 # Create public build directory for deployment
 mkdir -p public
 rsync -a src node_modules public
